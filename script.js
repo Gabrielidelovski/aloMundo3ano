b3cfede25ade1c0b8cb3a5f71a2917631b8ee105
Gabrielidelovski <164918814+Gabrielidelovski@users.noreply.github.com>
2025-08-06 13:24:11 -0300

    img

--- script.js
+++ script.js
@@ -10,9 +10,22 @@ const lista = document.querySelector('#lista')
 lista.textContent=linguagens[0];
 lista.textContent=linguagens[1];
 lista.textContent=linguagens[2];
+
 let aluno1={
     nome:'Gabrieli',
     idade:17,
     anoLetivo:'Cursando o ensino médio',
     materiaFavoritas:['Biologio', 'Educação Física', 'Matemática II']
 }
+console.log(alunos1.nome);
+console.log(aluno1.idade);
+console.log(aluno1.materiasFavoritas);
+console.log(aluno1.materiasFavoritas[1]);
+
+
+const ojeto = document.querySelector('#aluno');
+objeto.textContent = aluno1.nome;
+objeto.textContent = aluno1.idade;
+objeto.textContent = aluno1.materiasFavoritas[1];
+
+
